f723f18c8f01acb2341b61ddd3af4f14aaea0d5a
Jon Handy <jonhandy@gmail.com>
2017-07-11 03:37:56 +0000

    First Commit

--- collections_hierarchy/base_classes.py
+++ collections_hierarchy/base_classes.py
@@ -3,22 +3,60 @@ from .node import Node
 
 class Sequenceable(object):
     def __init__(self):
-        pass
+        self.start = None
+        self.end = None
 
     def get_elements(self):
-        pass
+        if not self.start:
+            return []
+        else:
+            current_node = self.start
+            
+            lst = []
+            lst.append(current_node.value)
+            while current_node.next:
+                current_node = current_node.next
+                lst.append(current_node.value)
+                
+                
+            return lst
+                
+            
+                
+            
 
 
 class Appendable(object):
     def append(self, element):
-        pass
+        node = Node(element)
+        if self.start is None:
+            self.start = node
+            self.end = node
+        else:
+            self.end.next = node
+            self.end = node
+            
+        
 
 
 class Popable(object):
     def pop(self):
-        pass
+        if self.start is None:
+            raise IndexError
+        
+        val = self.start.value
+        self.start = self.start.next
+        return val
+        
 
 
 class Pushable:
     def push(self, element):
-        pass
+        node = Node(element)
+        
+        if self.start is None:
+            self.start = node
+            self.end = self.start
+        else:
+            node.next = self.start
+            self.start = node
